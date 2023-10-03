792d5672c69b8345208e13cf1cdf9fcf40d114b5
Tobias Mitala <tobias.mitala@compote.eu>
2023-10-03 21:14:04 +0200

    Uloha 1

--- OptS/src/Main.java
+++ OptS/src/Main.java
@@ -1,5 +1,16 @@
+import CvičenieT2.Uloha7;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Uloha7 uloha7 = new Uloha7();
+        int [][] dji = {
+                {500, 100, 300, 400, 100},
+                {300, 200, 300, 400, 200},
+                {200, 200, 300, 100, 300},
+        };
+        int y[] = {1, 0, 1};
+        int zy[] = {0, 2};
+        System.out.println("Uloha 1 Hodnota účelovej funkcie je: " + uloha7.objUFLP_1(5, 3, y, new int[]{200, 300, 300}, new int[]{1, 1, 1, 1, 1}, dji));
+        System.out.println("Uloha 2 Hodnota účelovej funkcie je: " + uloha7.objUFLP_2(3, 5, 2, zy, new int[]{200, 300, 300}, new int[]{1, 1, 1, 1, 1}, dji));
     }
 }
